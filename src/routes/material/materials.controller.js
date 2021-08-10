5f7e29bb70140fe14f9fb5a09c27c642812045ad
Gonzalo Yubero Garcia <gonzaloyubero@gmail.com>
2021-08-10 17:32:48 +0200

    update query for materials done

--- src/routes/material/materials.controller.js
+++ src/routes/material/materials.controller.js
@@ -8,29 +8,29 @@ const Hotel = require('../../models').hotel;
  * @returns {ApiResponseList} reflection object
  */
  const getAllMaterials = async (condition) => {
-    try {
-      const result = await Material.findAll({
-        where: { ...condition },
-        attributes: ["id", "name", "quantity", "price", "average"],
-        include: [{
-            model: Hotel,
-            as: 'hotel',
-            attributes: ["id", "phone", "name", "mail"],
-        }]
+	try {
+		const result = await Material.findAll({
+        	where: { ...condition },
+        	attributes: ["id", "name", "quantity", "price", "average"],
+        	include: [{
+            	model: Hotel,
+            	as: 'hotel',
+            	attributes: ["id", "phone", "name", "mail"],
+        	}]
 
-      });
-      return {
-        success: true,
-        code: code.success,
-        lead: result,
-      };
+      	});
+      	return {
+        	success: true,
+        	code: code.success,
+        	lead: result,
+		};
     } catch (error) {
-        console.log(error);
-      return {
-        success: false,
-        code: code.error,
-        error: error,
-      };
+		console.log(error);
+		return {
+			success: false,
+        	code: code.error,
+        	error: error,
+		};
     }
 };
 
@@ -54,14 +54,39 @@ const addNewMaterial = async (material) => {
     } catch (error){
         console.log(error);
         return {
-          success: false,
-          code: code.error,
-          error: error,
+          	success: false,
+          	code: code.error,
+        	error: error,
         };
     }
 };
 
+const updateMaterial = async (material, id) => {
+  try {
+    const [numberOfAffectedRows, affectedRows] = await Material.update({
+        	name: material.name,
+        	quantity: material.quantity,
+        	price: material.price,
+        	average: material.average
+		},{where:{id: id}}
+    )
+    return{
+		success: true,
+		code: code.success,
+		lead: affectedRows
+    };
+	} catch (error) {
+		console.log(error);
+    	return {
+    		success: false,
+     		code: code.error,
+      		error: error,
+		};
+  	}
+};
+
 module.exports = {
     getAllMaterials,
-    addNewMaterial
+    addNewMaterial,
+	updateMaterial
 }
--- src/routes/material/materials.routes.js
+++ src/routes/material/materials.routes.js
@@ -1,4 +1,4 @@
-const { getAllMaterials, addNewMaterial } = require('./materials.controller');
+const { getAllMaterials, addNewMaterial, updateMaterial } = require('./materials.controller');
 
 const router = require('express').Router()
 
@@ -10,7 +10,6 @@ router.get('/', async (req, res) => {//retorna tots els materials
 
 // /materials/new
 router.post('/new', async (req, res) => {//crea material nou
-    console.log(req.body)
     const result = await addNewMaterial(req.body.material);
     res.send({ ...result})
 });
@@ -21,8 +20,9 @@ router.get('/delete/:id', (req, res) => {//elimina material
 });
 
 // /materials/update/{id}
-router.post('/update/:id', (req, res) => {//update material
-    res.json({ok: true})
+router.post('/update/:id', async (req, res) => {//update material
+    const result = await updateMaterial(req.body.material, req.params.id);
+    res.send({ ...result})
 });
 
 
